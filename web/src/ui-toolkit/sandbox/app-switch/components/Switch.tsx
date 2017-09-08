bd10a69d03713aa9c9696904955353b0184aeb60
VIKASSAR <VIKASSAR@VIKASSAR04>
2017-09-08 18:14:19 +0530

    switch button update

--- web/src/ui-toolkit/sandbox/app-switch/components/Switch.tsx
+++ web/src/ui-toolkit/sandbox/app-switch/components/Switch.tsx
@@ -19,11 +19,15 @@ export class Switch extends React.Component<ISwitchProps, ISwitchStates> {
     configs: any;
     translator: any;
 
+    switchChanged(isOn: boolean) {
+
+    }
+
     render(): React.ReactElement<ISwitchProps> {
        const template = (
            <p className="btn-switch">
-               <input type="radio" id="yes" name="switch" className="btn-switch__radio btn-switch__radio_yes" />
-               <input type="radio" checked id="no" name="switch" className="btn-switch__radio btn-switch__radio_no" />
+               <input type="radio" id="yes" name="switch" className="btn-switch__radio btn-switch__radio_yes" onChange={this.switchChanged.bind(this, true) }/>
+               <input type="radio" id="no" name="switch" className="btn-switch__radio btn-switch__radio_no" defaultChecked onChange={this.switchChanged.bind(this, false)}/>
                <label htmlFor ="yes" className="btn-switch__label btn-switch__label_yes"><span className="btn-switch__txt">Yes</span></label>
                <label htmlFor ="no" className="btn-switch__label btn-switch__label_no"><span className="btn-switch__txt">No</span></label>
            </p>
