6c1cc165d71cdf8a46c2ab847017cde0d67cb8c7
VIKASSAR <VIKASSAR@VIKASSAR04>
2017-09-08 10:40:47 +0530

    adding typings

---
+++ web/Scripts/typings/typings.d.ts
@@ -0,0 +1,39 @@
+/**
+ * Created by: Vikas sarkar
+ * date : 2017-07-15
+ */
+
+declare module "*.json" {
+    let json: any;
+    export default json;
+}
+
+declare module "*.scss" {
+    let scss: any;
+    export default scss;
+}
+
+declare module "*.css" {
+    let css: any;
+    export default css;
+}
+
+declare module "*.html" {
+    let html: any;
+    export default html;
+}
+
+declare module "*.jpg" {
+    let jpg: any;
+    export default jpg;
+}
+
+declare module "*.png" {
+    let png: any;
+    export default png;
+}
+
+declare module "*.svg" {
+    let svg: any;
+    export default svg;
+}
