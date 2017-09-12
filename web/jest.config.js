95d180dac140732ca7474b39f2b7b260be5749ac
VIKASSAR <VIKASSAR@VIKASSAR06>
2017-09-12 21:57:35 +0530

    adding seprate jest congfigs

---
+++ web/jest.config.js
@@ -0,0 +1,23 @@
+﻿module.exports = {
+    "verbose": true,
+    "collectCoverageFrom": [
+      "src/tests/**/**/*.ts",
+      "src/tests/**/**/*.tsx"
+    ],
+    "collectCoverage": true,
+    "coverageDirectory": "./src-output/tests-coverage/",
+    "moduleFileExtensions": [
+      "js",
+      "jsx",
+      "ts",
+      "tsx"
+    ],
+    "testRegex": "/src/tests/.*\\.(ts|tsx|js|jsx)$",
+    "setupTestFrameworkScriptFile": "mock-local-storage",
+    "transform": {
+        "^.+\\.(ts|tsx)$": "<rootDir>/node_modules/ts-jest/preprocessor.js"
+    },
+    "transformIgnorePatterns": [
+      "node_modules/"
+    ]
+};
